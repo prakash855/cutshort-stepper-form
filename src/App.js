566551eecc2682ac05a2115f07d24c7158f567d6
prakash855 <sshivamprakashh301@gmail.com>
2022-05-26 02:28:38 +0530

    feat: UI for stepper

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import UserForm from "./component/UserForm.jsx/UserForm";
 function App() {
   return (
     <div className="App">
+      <h2 className="eden">Eden</h2>
+      <div class="stepper-wrapper">
+        <div class="stepper-item completed">
+          <div class="step-counter">1</div>
+        </div>
+        <div class="stepper-item completed">
+          <div class="step-counter">2</div>
+        </div>
+        <div class="stepper-item active">
+          <div class="step-counter">3</div>
+        </div>
+        <div class="stepper-item">
+          <div class="step-counter">4</div>
+        </div>
+      </div>
       <UserForm />
     </div>
   );
